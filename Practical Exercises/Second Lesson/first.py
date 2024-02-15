fc8fd323822fbb7a6c1c90eb9c1f720cb631f869
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-02-16 01:25:08 +0300

    add folder for class and home practice
    add python scripts to folders

--- Practical Exercises/Second Lesson/first.py
+++
@@ -1,5 +0,0 @@
-# Дан список чисел Выведите все элементы списка с четными индексами (то
-# есть A[0], A[2], A[4], ...). В этой задаче нельзя использовать инструкцию if.
-
-list_input = list(map(float, input("input list items: ").split()))
-[print(list_input[i]) for i in range(1, len(list_input), 2)]
--- Practical Exercises/Second Lesson/third.py
+++
@@ -1,5 +0,0 @@
-# Дан список чисел. Определите, сколько в этом списке элементов, которые
-# больше двух своих соседей и выведите количество таких элементов.
-
-list_input = list(map(float, input("input list items: ").split()))
-[print(list_input[i]) for i in range(1, len(list_input) - 1) if list_input[i] > list_input[i+1] and list_input[i] > list_input[i-1] ]
