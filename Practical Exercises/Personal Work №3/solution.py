04dbe4bba5964c5a24a30331159461a3117d7490
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-03-23 16:43:09 +0300

    complete third personal task at first examples
    going to start testing another examples

---
+++ Practical Exercises/Personal Work №3/solution.py
@@ -0,0 +1,32 @@
+# В школу бальных танцев записались n учеников — мальчиков и девочек.
+# Руководитель школы построил их в один ряд,
+# и хочет отобрать из них для первого занятия группу стоящих подряд учеников,
+# в которой количество мальчиков и девочек одинаково.
+# Сколько вариантов выбора есть у профессора?
+
+# В первой строке файла задано число n
+# Во второй строке задается описание построенного ряда из мальчиков и девочек —
+# строка из nn символов a и b (символ a соответствует девочке, а символ b — мальчику).
+# Найти количество вариантов выбора требуемой группы
+
+# Например:
+# 8
+# abbababa
+# 13
+
+with open("input.txt") as input_file:
+    text = input_file.read().split('\n')
+    count = int(text[0])
+    pupils = text[1]
+    pairs = {}
+    for i in range(2, len(pupils) + 1, 2):
+        for j in range(len(pupils) - i + 1):
+            print(f"{pupils[j:i+j]}, i = {i}, j = {j}")
+            print(f"a = {pupils[j:i+j].count('a')}, b = {pupils[j:i+j].count('b')}")
+            if pupils[j:i+j].count('a') == pupils[j:i+j].count('b'):
+                if pupils[j:i+j] not in pairs.keys():
+                    pairs[pupils[j:i+j]] = 1
+                else:
+                    pairs[pupils[j:i + j]] += 1
+    print(pairs)
+    print(sum(pairs.values()))
