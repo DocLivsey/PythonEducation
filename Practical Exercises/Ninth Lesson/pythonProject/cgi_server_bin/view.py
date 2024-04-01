e65798e1f35853d4a397d83c9781e70781022897
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-04-01 18:36:13 +0300

    create local cgi server in create_cgi_sever.py add html form in index.html and listening it in view.py

---
+++ Practical Exercises/Ninth Lesson/pythonProject/cgi_server_bin/view.py
@@ -0,0 +1,18 @@
+import cgi
+
+form = cgi.FieldStorage()
+text1 = form.getfirst("FirstName", "не задано")
+text2 = form.getfirst("SurName", "не задано")
+print("Content-type: text/html\n")
+print("""<!DOCTYPE HTML>
+        <html>
+        <head>
+            <meta charset="cp1251">
+            <title>Обработка книг </title> 
+        </head>
+        <body>""")
+print("<h1>Обработка туристов</h1>")
+print("<p>Имя: {}</p>".format(text1))
+print("<p>Фамилия: {}</p>".format(text2))
+print("""</body> 
+</html>""")
