ee9406a8997fab96c4ba7977ec4aaf74a4de77dd
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-05-12 01:04:48 +0300

    add admin models of my app`s main django models

--- Practical Exercises/Personal Work №6 (Django The Unchained)/mysite/insurance/admin.py
+++ Practical Exercises/Personal Work №6 (Django The Unchained)/mysite/insurance/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class ClientsModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'passport']
+    list_display_links = ['passport']
+    search_fields = ['first_name', 'last_name', 'email', 'passport']
+    list_filter = ['first_name', 'last_name', 'email', 'passport']
+    list_editable = ['first_name', 'last_name', 'email']
+
+    class Meta:
+        model = Clients
+
+
+class InsuranceObjectModelAdmin(admin.ModelAdmin):
+    list_display = ['object_name', 'description']
+    list_display_links = ['object_name']
+    search_fields = ['object_name']
+    list_filter = ['object_name']
+    list_editable = ['description']
+
+    class Meta:
+        model = InsuranceObject
+
+
+class ContractsModelAdmin(admin.ModelAdmin):
+    list_display = ['client_id', 'object_id', 'contract_date', 'insurance_payment']
+    list_display_links = ['client_id', 'object_id', 'contract_date']
+    search_fields = ['client_id', 'object_id', 'contract_date', 'insurance_payment']
+    list_filter = ['client_id', 'object_id', 'contract_date', 'insurance_payment']
+    list_editable = ['insurance_payment']
+
+    class Meta:
+        model = Contracts
+
+
+class InsurancePaymentsModelAdmin(admin.ModelAdmin):
+    list_display = ['contract_id', 'date']
+    search_fields = ['contract_id', 'date']
+    list_filter = ['contract_id', 'date']
+
+
+admin.site.register(Clients, ClientsModelAdmin)
+admin.site.register(InsuranceObject, InsuranceObjectModelAdmin)
+admin.site.register(Contracts, ContractsModelAdmin)
+admin.site.register(InsurancePayments, InsurancePaymentsModelAdmin)
