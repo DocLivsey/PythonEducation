20589ed5cb9ece9835c95411313d63aa30c7d4c0
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-05-18 18:13:16 +0300

    add new paths

--- Practical Exercises/Personal Work №6 (Django The Unchained)/mysite/insurance/urls.py
+++ Practical Exercises/Personal Work №6 (Django The Unchained)/mysite/insurance/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login, name="login"),
+    path("login/", views.log_in, name="login"),
+    path("signup/", views.sign_up, name="signup"),
+    path("logout/", views.log_out, name='logout'),
     path("<int:id>/", views.clients_cabinet, name="clients_cabinet"),
     path("<int:id>/contract_details/", views.contract_detail, name="contract_details"),
-    path("<int:id>/make_order", views.order_insurance, name="make_order"),
-
+    path("<int:id>/make_order/", views.order_insurance, name="make_order"),
+    path("<int:id>/change_personal_data/", views.change_personal_data, name="change_personal_data"),
 ]
