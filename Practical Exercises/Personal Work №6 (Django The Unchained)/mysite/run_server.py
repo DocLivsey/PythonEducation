278d97d82374bbd6c2538ef0f1cb0fa1d55a396d
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-08-30 20:04:02 +0300

    create executable file to execute manage.py for running server

---
+++ Practical Exercises/Personal Work №6 (Django The Unchained)/mysite/run_server.py
@@ -0,0 +1,8 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+django.setup()
+
+from django.core.management import execute_from_command_line
+execute_from_command_line(["manage.py", "runserver"])
