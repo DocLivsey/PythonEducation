94450d6b1bbbc98d9503530375d816c2f52d6852
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-08-31 16:14:47 +0300

    make script for running server not via terminal with manage.py, but running like wsgi app

--- Practical Exercises/Personal Work №6 (Django The Unchained)/run_server.py
+++ Practical Exercises/Personal Work №6 (Django The Unchained)/run_server.py
@@ -1,13 +1,25 @@
 import os
 import sys
 
+# Отключаем автоперезагрузку Django
+os.environ['DJANGO_AUTORELOAD_DEBUG'] = 'false'
+
+# Добавляем путь к директории mysite
 sys.path.append(os.path.abspath('mysite'))
 
-import django
-
+# Устанавливаем настройки Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-django.setup()
 
-from django.core.management import execute_from_command_line
+# Импортируем WSGI-приложение
+from django.core.wsgi import get_wsgi_application
 
-execute_from_command_line(["manage.py", "runserver"])
+# Получаем WSGI-приложение
+application = get_wsgi_application()
+
+# Запускаем сервер с использованием WSGI
+from wsgiref.simple_server import make_server
+
+# Указываем адрес и порт
+server = make_server('127.0.0.1', 8000, application)
+print("Serving on http://127.0.0.1:8000/")
+server.serve_forever()
