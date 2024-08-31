ef43b26a77685eb01b1340cb1c5acfbdf8f6deee
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-08-31 15:30:40 +0300

    complete py script that execute another py script via terminal

--- Practical Exercises/Personal Work №6 (Django The Unchained)/run_server.py
+++ Practical Exercises/Personal Work №6 (Django The Unchained)/run_server.py
@@ -1,8 +1,13 @@
 import os
+import sys
+
+sys.path.append(os.path.abspath('mysite'))
+
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 django.setup()
 
 from django.core.management import execute_from_command_line
+
 execute_from_command_line(["manage.py", "runserver"])
